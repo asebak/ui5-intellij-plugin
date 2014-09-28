b81eb50e99077829321eb68ed3fc2fb7df18776a
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-09-28 07:11:27 -0400

    Update UI For project creator

--- src/ProjectBuilder/UI5ProjectPeer.java
+++ src/ProjectBuilder/UI5ProjectPeer.java
@@ -5,13 +5,25 @@ import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.platform.WebProjectGenerator;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
+import com.intellij.openapi.ui.*;
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by asebak on 9/27/2014.
  */
 public class UI5ProjectPeer implements WebProjectGenerator.GeneratorPeer<UI5ProjectTemplateGenerator.UI5ProjectSettings> {
+    private JPanel applicationTypeGroup =  new JPanel();
+    private JPanel viewTypeGroup=  new JPanel();
+    private ButtonGroup applicationTypeButtonGroup = new ButtonGroup();
+    private ButtonGroup viewTypeButtonGroup = new ButtonGroup();
+    private JRadioButton desktopTypeButton = new JRadioButton("Desktop");
+    private JRadioButton mobileTypeButton = new JRadioButton("Mobile");
+    private JRadioButton htmlViewButton = new JRadioButton("HTML");
+    private JRadioButton jsonViewButton = new JRadioButton("JSON");
+    private JRadioButton jsViewButton = new JRadioButton("Javascript");
+    private JRadioButton xmlViewButton = new JRadioButton("XML");
+
     @NotNull
     @Override
     public JComponent getComponent() {
@@ -20,6 +32,33 @@ public class UI5ProjectPeer implements WebProjectGenerator.GeneratorPeer<UI5Proj
 
     @Override
     public void buildUI(@NotNull SettingsStep settingsStep) {
+        initializeViews();
+        settingsStep.addSettingsField("Project Type:", applicationTypeGroup);
+        settingsStep.addSettingsField("Initial View Type:", viewTypeGroup);
+    }
+
+    private void initializeViews(){
+        applicationTypeGroup.setLayout(new FlowLayout());
+        viewTypeGroup.setLayout(new FlowLayout());
+
+        //Default Selection
+        desktopTypeButton.setSelected(true);
+        jsViewButton.setSelected(true);
+        //Button Groups to Handle Logic Selection
+        applicationTypeGroup.add(desktopTypeButton);
+        applicationTypeGroup.add(mobileTypeButton);
+        viewTypeButtonGroup.add(htmlViewButton);
+        viewTypeButtonGroup.add(jsonViewButton);
+        viewTypeButtonGroup.add(jsViewButton);
+        viewTypeButtonGroup.add(xmlViewButton);
+
+        //JPanel Add Views
+        applicationTypeButtonGroup.add(desktopTypeButton);
+        applicationTypeButtonGroup.add(mobileTypeButton);
+        viewTypeGroup.add(jsViewButton);
+        viewTypeGroup.add(xmlViewButton);
+        viewTypeGroup.add(htmlViewButton);
+        viewTypeGroup.add(jsonViewButton);
 
     }
 
