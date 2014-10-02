7497450e3bea56416bb19e70070073e395d56b03
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-10-02 04:52:28 -0400

    Added file factory

---
+++ src/FileBuilder/UI5FileFactory.java
@@ -0,0 +1,16 @@
+package FileBuilder;
+
+import com.intellij.openapi.fileTypes.FileTypeConsumer;
+import com.intellij.openapi.fileTypes.FileTypeFactory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by asebak on 10/1/2014.
+ */
+public class UI5FileFactory extends FileTypeFactory {
+    @Override
+    public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
+
+//       fileTypeConsumer.consume(null);
+    }
+}
