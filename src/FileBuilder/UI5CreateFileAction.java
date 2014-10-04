9f633db76fd28a24596e44a3930553a3c033e4f3
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-10-04 16:02:37 -0400

    Updated Icons Design in File

--- src/FileBuilder/UI5CreateFileAction.java
+++ src/FileBuilder/UI5CreateFileAction.java
@@ -1,6 +1,7 @@
 package FileBuilder;
 
 import Util.UI5Icons;
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.actions.CreateFileFromTemplateAction;
 import com.intellij.ide.actions.CreateFileFromTemplateDialog;
 import com.intellij.openapi.project.DumbAware;
@@ -26,13 +27,13 @@ public class UI5CreateFileAction extends CreateFileFromTemplateAction implements
 
     @Override
     protected void buildDialog(Project project, PsiDirectory directory, CreateFileFromTemplateDialog.Builder builder) {
-        builder
-                .setTitle("UI5 Component")
-                .addKind("Javascript View", UI5Icons.getIcon(), "ColdFusion File.cfm")
-                .addKind("HTML View", UI5Icons.getIcon(), "ColdFusion Script Component.cfc")
-                .addKind("JSON View", UI5Icons.getIcon(), "ColdFusion Script Interface.cfc")
-                .addKind("XML View", UI5Icons.getIcon(), "ColdFusion Tag Component.cfc")
-                .addKind("i18n", UI5Icons.getIcon(), "ColdFusion Tag Interface.cfc");
+
+        builder.setTitle("UI5 Component")
+                .addKind("Javascript View", AllIcons.FileTypes.JavaScript, "ColdFusion File.cfm")
+                .addKind("HTML View", AllIcons.FileTypes.Html, "ColdFusion Script Component.cfc")
+                .addKind("JSON View", AllIcons.FileTypes.Json, "ColdFusion Script Interface.cfc")
+                .addKind("XML View", AllIcons.FileTypes.Xml, "ColdFusion Tag Component.cfc")
+                .addKind("i18n", AllIcons.FileTypes.Properties, "ColdFusion Tag Interface.cfc");
     }
 
     @Override
