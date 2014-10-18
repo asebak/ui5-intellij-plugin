9d89b200d6654b95d336b7836882b3a039f5e017
ahmad sebak <rhymeweaponlegend@gmail.com>
2014-10-18 14:36:53 -0400

    Added backgorund task for file creation but its still slow

--- src/FileBuilder/UI5CreateFileAction.java
+++ src/FileBuilder/UI5CreateFileAction.java
@@ -8,6 +8,8 @@ import com.intellij.ide.actions.CreateFileFromTemplateAction;
 import com.intellij.ide.actions.CreateFileFromTemplateDialog;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
@@ -33,67 +35,80 @@ public class UI5CreateFileAction extends CreateFileFromTemplateAction implements
     }
 
     @Override
-    protected void postProcess(PsiFile createdElement, String templateName, Map<String, String> customProperties) {
+    protected void postProcess(final PsiFile createdElement, final String templateName, Map<String, String> customProperties) {
         //TODO implement it as a background task
-        String[] templatePattern = templateName.split(Pattern.quote("."));
-        String ext = templatePattern[templatePattern.length - 1];
-        String createdName = createdElement.getName();
-        String newCreatedName = createdName.replace("." + ext, "") + ".view." + ext;
-        createdElement.setName(newCreatedName);
-        String viewFilePath = createdElement.getVirtualFile().getParent().getPath() + "\\" + newCreatedName;
-        String controllerFilePath = createdElement.getVirtualFile().getParent().getPath() + "\\" + createdName.replace("." + ext, "") + ".controller.js";
-        File controllerFile = new File(controllerFilePath);
-        File viewFile = new File(viewFilePath);
-        UI5View ui5View = null;
-        if (ext.equals("js")) {
-            ui5View = new JSView();
-        } else if (ext.equals("xml")) {
-            ui5View = new XMLView();
-        } else if (ext.equals("json")) {
-            ui5View = new JSONView();
-        } else if (ext.equals("html")) {
-            ui5View = new HTMLView();
-        } else {
-
+        if (templateName == "ui5.properties") {
+            return;
         }
+        ProgressManager.getInstance().runProcessWithProgressSynchronously(new Runnable() {
+            @Override
+            public void run() {
+                //setting path
+                ProgressIndicator indicator = ProgressManager.getInstance().getProgressIndicator();
+                indicator.setText("Creating UI5 View");
+                indicator.setFraction(10);
+                String[] templatePattern = templateName.split(Pattern.quote("."));
+                String ext = templatePattern[templatePattern.length - 1];
+                String createdName = createdElement.getName();
+                String newCreatedName = createdName.replace("." + ext, "") + ".view." + ext;
+                createdElement.setName(newCreatedName);
+                String viewFilePath = createdElement.getVirtualFile().getParent().getPath() + "\\" + newCreatedName;
+                String controllerFilePath = createdElement.getVirtualFile().getParent().getPath() + "\\" + createdName.replace("." + ext, "") + ".controller.js";
+                File controllerFile = new File(controllerFilePath);
+                File viewFile = new File(viewFilePath);
+                UI5View ui5View = null;
+                indicator.setFraction(30);
+                //which view is created
+                if (ext.equals("js")) {
+                    ui5View = new JSView();
+                } else if (ext.equals("xml")) {
+                    ui5View = new XMLView();
+                } else if (ext.equals("json")) {
+                    ui5View = new JSONView();
+                } else if (ext.equals("html")) {
+                    ui5View = new HTMLView();
+                } else {
 
-        Module module = ModuleUtilCore.findModuleForPsiElement(createdElement);
-//        createdElement.getOriginalFile().delete();
-        if (module != null) {
-            String[] projectDircs;
-            String[] fileDircs;
-            String projectPath = module.getProject().getBasePath();
-            String filePath = new File(createdElement.getVirtualFile().getParent().getPath()).getPath();
-            projectDircs = projectPath.split(Pattern.quote(File.separator));
-            fileDircs = filePath.split(Pattern.quote(File.separator));
-            for (int i = 0; i < projectDircs.length; i++) {
-                if (projectDircs[i].equals(fileDircs[i])) {
-                    fileDircs[i] = "";
                 }
-            }
-            String codePath = "";
-            for (int i = 0; i < fileDircs.length; i++) {
-                if (!fileDircs[i].isEmpty()) {
-                    codePath += fileDircs[i];
-                    if (i != fileDircs.length - 1) {
-                        codePath += ".";
+
+                //get the module path for autogeneration
+                Module module = ModuleUtilCore.findModuleForPsiElement(createdElement);
+                createdElement.getOriginalFile().delete();
+                if (module != null) {
+                    String[] projectDircs;
+                    String[] fileDircs;
+                    String projectPath = module.getProject().getBasePath();
+                    String filePath = new File(createdElement.getVirtualFile().getParent().getPath()).getPath();
+                    projectDircs = projectPath.split(Pattern.quote(File.separator));
+                    fileDircs = filePath.split(Pattern.quote(File.separator));
+                    for (int i = 0; i < projectDircs.length; i++) {
+                        if (projectDircs[i].equals(fileDircs[i])) {
+                            fileDircs[i] = "";
+                        }
+                    }
+                    String codePath = "";
+                    for (int i = 0; i < fileDircs.length; i++) {
+                        if (!fileDircs[i].isEmpty()) {
+                            codePath += fileDircs[i];
+                            if (i != fileDircs.length - 1) {
+                                codePath += ".";
+                            }
+                        }
+
+                    }
+                    String viewCode = ui5View.autogenerateCode(UI5Library.Desktop, codePath + "." + createdName.replace("." + ext, ""));
+                    String controllerCode = Controller.getAutogenerateCode(codePath, createdName.replace("." + ext, ""));
+                    try {
+                        Writer.writeToFile(controllerFile, controllerCode);
+                        Writer.writeToFile(viewFile, viewCode);
+                    } catch (IOException e) {
+                        Messages.showErrorDialog(createdElement.getProject(), e.getMessage(), "Create File from Template");
+                        e.printStackTrace();
                     }
                 }
-
             }
-            String viewCode = ui5View.autogenerateCode(UI5Library.Desktop, codePath + "." + createdName.replace("." + ext, ""));
-            String controllerCode = Controller.getAutogenerateCode(codePath, createdName.replace("." + ext, ""));
-            try {
-                Writer.writeToFile(controllerFile, controllerCode);
-                Writer.writeToFile(viewFile, viewCode);
-            } catch (IOException e) {
-                Messages.showErrorDialog(createdElement.getProject(), e.getMessage(), "Create File from Template");
-                e.printStackTrace();
-            }
-        }
-
-
-        super.postProcess(createdElement, templateName, customProperties);
+        }, "Adding UI5 View", false, createdElement.getProject());
+//        super.postProcess(createdElement, templateName, customProperties);
     }
 
     @Override
