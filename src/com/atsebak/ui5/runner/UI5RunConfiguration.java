c9ea5827dd2338648a0cbdc6264f12fd4e90d7f9
asebak <rhymeweaponlegend@gmail.com>
2015-06-12 01:35:29 -0400

    removed url check

--- src/com/atsebak/ui5/runner/UI5RunConfiguration.java
+++ src/com/atsebak/ui5/runner/UI5RunConfiguration.java
@@ -1,6 +1,5 @@
 package com.atsebak.ui5.runner;
 
-import com.atsebak.ui5.locale.UI5Bundle;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.Executor;
 import com.intellij.execution.configuration.EmptyRunProfileState;
@@ -15,26 +14,12 @@ import lombok.Getter;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 @Getter
 public class UI5RunConfiguration extends RunConfigurationBase {
     private UI5RunnerParameters runnerParameters = new UI5RunnerParameters();
 
     protected UI5RunConfiguration(@NotNull Project project, @NotNull ConfigurationFactory factory, @NotNull String name) {
         super(project, factory, name);
-    }
-
-    public static void checkURL(String url) throws RuntimeConfigurationException {
-        try {
-            if (url == null) {
-                throw new MalformedURLException(UI5Bundle.getString("url.invalid"));
-            }
-            new URL(url);
-        } catch (MalformedURLException ignored) {
-            throw new RuntimeConfigurationError(UI5Bundle.getString("url.exception"));
-        }
     }
 
     @NotNull
@@ -69,7 +54,6 @@ public class UI5RunConfiguration extends RunConfigurationBase {
 
     @Override
     public void checkConfiguration() throws RuntimeConfigurationException {
-//        checkURL(runnerParameters.getUrl());
     }
 
 }
